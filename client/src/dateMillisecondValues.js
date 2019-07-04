5fba2a19f58506edc65875737aa6d307903d95db
mac718 <mac718@gmail.com>
2019-07-04 14:48:39 -0700

    fix issues in getFilteredTransactions and dateWidget

--- client/src/dateMillisecondValues.js
+++ client/src/dateMillisecondValues.js
@@ -1,4 +1,4 @@
 export const dataStartDate = 1480550400000;
-export const widgetStartDate = 1483228800000;
+export const widgetStartDate = 1483315200000//483228800000;
 export const endDate = 1514764800000;
 export const dayLength = 86400000;
