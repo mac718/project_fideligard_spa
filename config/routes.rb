d323153ec638e977e57620709d1ac1ad6410b23d
Michael Coon <mac718@gmail.com>
2019-09-19 16:28:31 -0700

    additional config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/api/historical', to: 'quandl#historical'
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
