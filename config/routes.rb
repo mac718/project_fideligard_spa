9ecc1fbbc34f57316b802731fd6fbff79de6a5c2
mac718 <mac718@gmail.com>
2019-09-21 15:54:57 -0700

    root route in back end

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  #get '/', to: 'quandl#historical'
+  get '/', to: 'quandl#historical'
   get '/api/historical', to: 'quandl#historical'
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
