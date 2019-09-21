a037bdebc956511d2ad926d0d991cd3956d2fd12
mac718 <mac718@gmail.com>
2019-09-21 16:13:32 -0700

    add back api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #get '/', to: 'quandl#historical'
-  #get '/api/historical', to: 'quandl#historical'
+  get '/api/historical', to: 'quandl#historical'
   get '/', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
