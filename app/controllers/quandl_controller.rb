50fea36b341cf2c85643d2785665d83d582b5d2e
mac718 <mac718@gmail.com>
2019-10-08 16:53:18 -0700

    remove comment

--- app/controllers/quandl_controller.rb
+++ app/controllers/quandl_controller.rb
@@ -2,7 +2,7 @@ class QuandlController < ApplicationController
   def historical
     key = Figaro.env.QUANDL_API_KEY
     @results = []
-    datasetCode = ['FB', 'BA', 'AAPL', 'GM'] #create method that returns array
+    datasetCode = ['FB', 'BA', 'AAPL', 'GM']
     datasetCode.each do |code|
       @results.push(HTTParty.get("https://www.quandl.com/api/v3/datasets/WIKI/#{code}/data.json?start_date='2016-12-01'&end_date='2017-12-31'&api_key=#{key}"))
     end
